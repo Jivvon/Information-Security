f3b54431014d9abf7d77dcf9b2bb11f094cd6566
Jiwon Jeong <poiu8944@gmail.com>
2020-09-16 11:45:48 +0900

    3주차 과제 완료

--- week_3/des.py
+++ week_3/des.py
@@ -1,25 +1,26 @@
 from Crypto.Cipher import DES
 
-
 def encrypt_des(message: str, key: str, mode: int, iv=None) -> (bytes, bytes):
     """
 
-    :param message:
-    :param key:
-    :param mode:
-    :param iv:
+    :param message: 암호화할 평문
+    :param key: 대칭키.
+    :param mode: 블록 암호 모드. 평문 블록과 암호 알고리즘의 출력을 XOR하여 암호문 블록을 만들어내는 OFB(Output-FeedBack mode)를 사용한다.
+    :param iv: initial vector, 특정 계산함수를 반복 수행하는 DES에서는 첫 연산은 이전 연산값이 없으므로 이를 대신한다.
     :return:
     """
-    pass
+    cipher = DES.new(bytes(key, 'utf-8'), mode, iv=iv) # bytes 객체로 변환한 key와 mode, iv로 암호화하기 위한 DES 객체를 만든다.
+    return cipher.iv, cipher.encrypt(bytes(message, 'utf-8')) # cipher 객체를 사용하여 message를 암호화하여 iv와 함께 반환한다.
 
 
 def decrypt_des(encrypted: bytes, key: str, mode: int, iv: bytes) -> str:
     """
 
-    :param encrypted:
-    :param key:
-    :param mode:
-    :param iv:
-    :return:
+    :param encrypted: 암호화된 bytes 객체
+    :param key: 대칭키
+    :param mode: 블록 암호 모드. 평문 블록과 암호 알고리즘의 출력을 XOR하여 암호문 블록을 만들어내는 OFB(Output-FeedBack mode)를 사용한다.
+    :param iv: initial vector, 이를 이용해서 암호화를 시작하였으므로, 복호화할 때에도 필요하다
+    :return: key와 iv를 이용하여 encrypted를 복호화한 값
     """
-    pass
+    cipher = DES.new(bytes(key, 'utf-8'), mode, iv=iv) # bytes 객체로 변환한 key와 mode, iv로 복호화하기 위한 DES 객체를 만든다.
+    return cipher.decrypt(encrypted).decode('utf-8') # cipher 객체를 사용하여 암호화된 encrypted를 복호화하여 반환한다. 이 때, DES 객체의 iv와 key를 사용한다.
--- week_3/xor.py
+++ week_3/xor.py
@@ -1,7 +1,6 @@
 from typing import List
 from bitarray import bitarray
 
-
 def string_to_bits(string: str) -> List[bool]:
     bits = bitarray()
     bits.frombytes(string.encode('utf-8'))
@@ -15,10 +14,18 @@ def bits_to_string(bits: List[bool]) -> str:
 def xor_encrypt_decrypt(message: str, key: str) -> str:
     """
 
-    :param message:
-    :param key:
-    :return:
+    :param message: 암호화, 복호화할 평문.
+    :param key: 암호화, 복호화할 때 사용할 key.
+    :return: 암호화, 복호화한 문자열.
     """
     message_bits = string_to_bits(message)
     key_bits = string_to_bits(key)
-    pass
+    
+    answer = []
+    for i, ch in enumerate(message_bits): # 평문을 bit로 바꾼 것을 순회한다.
+        # 각 문자 순서대로, key를 bit로 바꾼 key_bits 또한 순서대로 순회하며 XOR 연산을 하고 answer에 값을 넣어준다.
+        answer.append(ch ^ key_bits[i % len(key_bits)])
+
+    return bits_to_string(answer)
+        
+    
