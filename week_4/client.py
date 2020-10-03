c1d79ce6dd5297919e5c4465f7e4ae5c895f08d4
Jiwon Jeong <poiu8944@gmail.com>
2020-10-04 02:18:53 +0900

    4주차 과제 완료

--- week_4/client.py
+++ week_4/client.py
@@ -7,8 +7,8 @@ from Crypto.Cipher import AES
 def pad(s: str):
     """
 
-    :param s:
-    :return:
+    :param s: padding할 평문
+    :return: padding한 문자열
     """
     return s + (16 - len(s) % 16) * chr(16 - len(s) % 16)
 
@@ -16,8 +16,8 @@ def pad(s: str):
 def unpad(s: bytes):
     """
 
-    :param s:
-    :return:
+    :param s: padding된 문자열
+    :return: padding되지 않은 평문
     """
     return s[0:-s[-1]]
 
@@ -25,34 +25,40 @@ def unpad(s: bytes):
 def encrypt(data: str, key: bytes) -> bytes:
     """
 
-    :param data:
-    :param key:
-    :return:
+    :param data: 평문
+    :param key: 16 bytes key (암복호화할 때 같아야 함)
+    :return: initial vector + 암호화된 평문 data
     """
     #  TODO: 구현할 부분 (data.encode('utf-8') 도 변경해도 됨)
+    data = pad(data)
+    iv = pad('initialvector').encode('utf-8')
+    cipher = AES.new(key, AES.MODE_CBC, iv )
+    data = iv + cipher.encrypt(data.encode('utf-8'))
 
-    return data.encode('utf-8')
+    return data
 
 
 def decrypt(data: bytes, key: bytes) -> str:
     """
 
-    :param data:
-    :param key:
-    :return:
+    :param data: initial vector(16) + 암호화된 data
+    :param key: 16 bytes key (암복호화할 때 같아야 함)
+    :return: 평문 (parameter data에서 initial vector를 제외하고 남은 부분을 복호화한 문자열)
     """
     #  TODO: 구현할 부분 (data.decode('utf-8') 도 변경해도 됨)
-
+    iv, data = data[:16], data[16:]
+    cipher = AES.new(key, AES.MODE_CBC, iv )
+    data = unpad(cipher.decrypt(data))
+    
     return data.decode('utf-8')
 
 
 def send(sock, key):
     """
-    함수설명:
+    함수설명: 사용자의 입력을 계속 기다린다. 사용자가 데이터를 입력하면 key와 함께 암호화하여 sock을 통해 보낸다.
 
-    :param sock:
-    :param key:
-    :return:
+    :param sock: 클라이언트 소켓 객체
+    :param key: byte 형식, 16 bytes로 인코딩된 key
     """
     while True:
         send_data = input('>>>')
@@ -61,11 +67,11 @@ def send(sock, key):
 
 def receive(sock, key):
     """
-    함수설명:
+    # socket.recv(1024) : socket의 버퍼에서 1024 bytes만큼의 데이터를 읽는다. 없다면 올 때까지 기다린다.
+    함수설명: sock에서 읽을 데이터가 있을 때까지(버퍼) 계속 기다린다. sock의 버퍼에 데이터가 생기면 key를 이용해 복호화한 후 출력한다.
 
-    :param sock:
-    :param key:
-    :return:
+    :param sock: 클라이언트 소켓 객체
+    :param key: byte 형식, 16 bytes로 인코딩된 key
     """
     while True:
         recv_data = sock.recv(1024)
--- week_4/server.py
+++ week_4/server.py
@@ -5,23 +5,22 @@ import time
 
 def send(sock, send_data):
     """
-    함수설명:
+    함수설명: sock으로 send_data를 전송한다.
 
-    :param sock:
-    :param send_data:
-    :return:
+    :param sock: 데이터를 보낼 소켓
+    :param send_data: sock으로 전송할 데이터. byte 형식. 암호화되어있어 읽을 수 없다.
     """
     sock.send(send_data)
 
 
 def receive(sock, addr, dst):
     """
-    함수설명:
+    # socket.recv(1024) : socket의 버퍼에서 1024 bytes만큼의 데이터를 읽는다. 없다면 올 때까지 기다린다.
+    함수설명: sock 소켓으로 데이터가 오는지 기다린다(sock.recv). 데이터가 온다면 보낸 클라이언트의 주소와 데이터를 출력한 후, dst 소켓으로 데이터를 보낸다.
 
-    :param sock:
-    :param addr:
-    :param dst:
-    :return:
+    :param sock: 데이터를 읽을 소켓. 이 소켓으로 오는 데이터를 읽는다.
+    :param addr: 데이터를 읽을 소켓의 주소
+    :param dst: 받은 데이터를 전송할 소켓
     """
     while True:
         recv_data = sock.recv(1024)
